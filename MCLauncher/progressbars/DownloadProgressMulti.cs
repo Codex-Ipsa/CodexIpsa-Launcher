83693ba1b775140686133954a0443c5fa7a9cdad
DEJVOSS <63589160+IronBeagle2@users.noreply.github.com>
2022-12-31 13:05:24 +0100

    Update DownloadProgressMulti.cs

--- MCLauncher/progressbars/DownloadProgressMulti.cs
+++ MCLauncher/progressbars/DownloadProgressMulti.cs
@@ -14,6 +14,11 @@ namespace MCLauncher.progressbars
     public partial class DownloadProgressMulti : Form
     {
         WebClient client;
+
+        List<string> theUrls = new List<string>();
+        List<string> thePaths = new List<string>();
+        int theSize;
+
         public DownloadProgressMulti(List<string> urls, List<string> paths, int totalSize)
         {
             InitializeComponent();
@@ -22,17 +27,24 @@ namespace MCLauncher.progressbars
             this.MinimizeBox = false;
             this.ControlBox = false;
             progressBarDownload.Maximum = totalSize;
+
+            theUrls = urls;
+            thePaths = paths;
+            theSize = totalSize;
+
             client = new WebClient();
             client.DownloadProgressChanged += new DownloadProgressChangedEventHandler(client_DownloadProgressChanged);
-            startDownload(urls, paths, totalSize);
+            startDownload();
         }
 
-        public void startDownload(List<string> urls, List<string> paths, int totalSize)
+        public void startDownload()
         {
             int i = 0;
-            foreach(String url in urls)
+            foreach(String url in theUrls)
             {
-                client.DownloadFileAsync(new Uri(url), paths[i]);
+                Console.WriteLine($"Downloading {url}");
+                client.DownloadFile(new Uri(url), thePaths[i]);
+
                 //TODO: DOWWNLOAD FILE COMPLETED LIKE IN THE ACCEPTED ANSWER
                 //https://stackoverflow.com/questions/2042258/webclient-downloadfileasync-download-files-one-at-a-time
                 i++;
@@ -40,11 +52,11 @@ namespace MCLauncher.progressbars
         }
 
         void client_DownloadProgressChanged(object sender, DownloadProgressChangedEventArgs e)
-        { 
+        {
             progressBarDownload.Value = (int)e.BytesReceived;
         }
 
-            private void cancelBtn_Click(object sender, EventArgs e)
+        private void cancelBtn_Click(object sender, EventArgs e)
         {
 
         }
