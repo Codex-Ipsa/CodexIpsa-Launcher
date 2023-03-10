8a517bfe10c7ef625b5375e345541ae4fecfc50d
DEJVOSS <63589160+IronBeagle2@users.noreply.github.com>
2023-03-10 18:01:49 +0100

    Thing or something or

--- MCLauncher/InstanceManager.Designer.cs
+++ MCLauncher/InstanceManager.Designer.cs
@@ -100,6 +100,11 @@ namespace MCLauncher
             this.modView = new System.Windows.Forms.ListView();
             this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.columnHeader5 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label11 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
             this.javaPage.SuspendLayout();
             this.grbInfo.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.minRamBox)).BeginInit();
@@ -110,6 +115,7 @@ namespace MCLauncher
             this.mainPage.SuspendLayout();
             this.verPage.SuspendLayout();
             this.modsPage.SuspendLayout();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // opendirBtn
@@ -544,6 +550,8 @@ namespace MCLauncher
             // 
             // mainPage
             // 
+            this.mainPage.Controls.Add(this.groupBox2);
+            this.mainPage.Controls.Add(this.groupBox1);
             this.mainPage.Controls.Add(this.label10);
             this.mainPage.Controls.Add(this.label1);
             this.mainPage.Location = new System.Drawing.Point(4, 22);
@@ -556,7 +564,7 @@ namespace MCLauncher
             // label10
             // 
             this.label10.AutoSize = true;
-            this.label10.Location = new System.Drawing.Point(178, 104);
+            this.label10.Location = new System.Drawing.Point(194, 323);
             this.label10.Name = "label10";
             this.label10.Size = new System.Drawing.Size(238, 13);
             this.label10.TabIndex = 1;
@@ -565,11 +573,11 @@ namespace MCLauncher
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(178, 91);
+            this.label1.Location = new System.Drawing.Point(194, 310);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(183, 13);
+            this.label1.Size = new System.Drawing.Size(180, 13);
             this.label1.TabIndex = 0;
-            this.label1.Text = "This is still very earlyt in development.";
+            this.label1.Text = "This is still very early in development.";
             // 
             // verPage
             // 
@@ -904,6 +912,52 @@ namespace MCLauncher
             this.columnHeader5.Text = "Type";
             this.columnHeader5.Width = 77;
             // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.label11);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Location = new System.Drawing.Point(8, 3);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(644, 92);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Profile";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(165, 19);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(473, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(94, 22);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(65, 13);
+            this.label11.TabIndex = 4;
+            this.label11.Text = "Profile name";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(15, 22);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(60, 60);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "(icon)";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Location = new System.Drawing.Point(8, 101);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(644, 136);
+            this.groupBox2.TabIndex = 3;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Game";
+            // 
             // InstanceManager
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -933,6 +987,8 @@ namespace MCLauncher
             this.verPage.ResumeLayout(false);
             this.verPage.PerformLayout();
             this.modsPage.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -1008,5 +1064,10 @@ namespace MCLauncher
         private System.Windows.Forms.Button btnOpen;
         private System.Windows.Forms.ColumnHeader columnHeader5;
         public System.Windows.Forms.ComboBox verBox;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button button1;
     }
 }
