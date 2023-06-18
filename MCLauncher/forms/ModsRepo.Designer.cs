3a00d0885462753a94014b1ca0baa1c1c1b96800
DEJVOSS <63589160+IronBeagle2@users.noreply.github.com>
2023-06-18 13:04:03 +0200

    Why is this resizable

--- MCLauncher/forms/ModsRepo.Designer.cs
+++ MCLauncher/forms/ModsRepo.Designer.cs
@@ -106,7 +106,9 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(331, 310);
             this.Controls.Add(this.tabControl1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
             this.Name = "ModsRepo";
             this.Text = "Mod repositories";
             this.tabControl1.ResumeLayout(false);
