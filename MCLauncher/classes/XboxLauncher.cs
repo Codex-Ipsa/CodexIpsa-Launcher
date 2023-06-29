f0cf7673eebd0402087dae23d7fdd9cd459849e7
DEJVOSS <63589160+IronBeagle2@users.noreply.github.com>
2023-06-29 13:01:25 +0200

    Finish XboxLauncher

--- MCLauncher/classes/XboxLauncher.cs
+++ MCLauncher/classes/XboxLauncher.cs
@@ -19,12 +19,12 @@ namespace MCLauncher.classes
             //download/update xenia
             Directory.CreateDirectory($"{Globals.dataPath}\\emulator\\xenia\\");
             var xm = JsonConvert.DeserializeObject<XeniaManifest>(Globals.client.DownloadString(Globals.xeniaManifest));
-            if(File.Exists($"{Globals.dataPath}\\emulator\\xenia\\version.cfg"))
+            if (File.Exists($"{Globals.dataPath}\\emulator\\xenia\\version.cfg"))
             {
                 File.Move($"{Globals.dataPath}\\emulator\\xenia\\version.cfg", $"{Globals.dataPath}\\emulator\\xenia\\version");
             }
 
-            if(!File.Exists($"{Globals.dataPath}\\emulator\\xenia\\version") || !File.ReadAllText($"{Globals.dataPath}\\emulator\\xenia\\version").Contains(xm.ver))
+            if (!File.Exists($"{Globals.dataPath}\\emulator\\xenia\\version") || !File.ReadAllText($"{Globals.dataPath}\\emulator\\xenia\\version").Contains(xm.ver))
             {
                 File.Delete($"{Globals.dataPath}\\emulator\\xenia\\xenia_canary.exe");
                 File.Delete($"{Globals.dataPath}\\emulator\\xenia\\LICENSE");
@@ -62,24 +62,69 @@ namespace MCLauncher.classes
             //launching bases
             if (dj.version.Contains("pre") || dj.version == "tu0")
             {
+                if(Directory.Exists($"{Globals.dataPath}\\emulator\\xenia\\content\\584111F7\\000B0000\\"))
+                    Directory.Delete($"{Globals.dataPath}\\emulator\\xenia\\content\\584111F7\\000B0000\\", true);
+
                 Process proc = new Process();
                 proc.EnableRaisingEvents = true;
                 proc.StartInfo.FileName = $"\"{Globals.dataPath}\\emulator\\xenia\\xenia_canary.exe\"";
                 proc.StartInfo.Arguments = $"\"{Globals.dataPath}\\versions\\x360\\{dj.version}\\default.xex\"";
                 proc.Exited += OnProcessExited;
                 proc.Start();
+                Logger.Info("[XboxLauncher]", "Launched xenia @ base");
                 Discord.ChangeMessage($"Playing Xbox 360 Edition ({dj.version})");
             }
             //launching updates
             else
             {
                 //download base
-                if (!File.Exists($"{Globals.dataPath}\\versions\\x360\\tu0.zip"))
+                if (!Directory.Exists($"{Globals.dataPath}\\versions\\x360\\tu0"))
                 {
                     DownloadProgress.url = Globals.client.DownloadString(Globals.x360Base);
                     DownloadProgress.savePath = $"{Globals.dataPath}\\versions\\x360\\tu0.zip";
                     DownloadProgress download = new DownloadProgress();
                     download.ShowDialog();
+
+                    ZipFile.ExtractToDirectory($"{Globals.dataPath}\\versions\\x360\\tu0.zip", $"{Globals.dataPath}\\versions\\x360\\tu0");
+                    File.Delete($"{Globals.dataPath}\\versions\\x360\\tu0.zip");
+                }
+
+                if (!Directory.Exists($"{Globals.dataPath}\\emulator\\xenia\\content\\584111F7\\000B0000\\{dj.version}"))
+                {
+                    Logger.Info("[XboxLauncher]", "Copying update files...");
+                    Directory.CreateDirectory($"{Globals.dataPath}\\emulator\\xenia\\content\\584111F7\\000B0000");
+                    DirectoryInfo di = new DirectoryInfo($"{Globals.dataPath}\\emulator\\xenia\\content\\584111F7\\000B0000");
+                    foreach (FileInfo file in di.GetFiles())
+                    {
+                        file.Delete();
+                    }
+                    foreach (DirectoryInfo dir in di.GetDirectories())
+                    {
+                        dir.Delete(true);
+                    }
+                    Directory.CreateDirectory($"{Globals.dataPath}\\emulator\\xenia\\content\\584111F7\\000B0000\\{dj.version}");
+
+                    DirectoryInfo itunes = new DirectoryInfo($"{Globals.dataPath}\\versions\\x360\\{dj.version}");
+                    FileInfo[] itunesFiles = itunes.GetFiles("*.*", SearchOption.AllDirectories);
+                    foreach(FileInfo file in itunesFiles)
+                    {
+                        string fileName = file.FullName.Replace($"{Globals.dataPath}\\versions\\x360\\{dj.version}", "");
+                        string path = fileName.Substring(0, fileName.LastIndexOf("\\") + 1);
+                        Console.WriteLine(file.FullName);
+                        Console.WriteLine(path + fileName);
+                        Console.WriteLine("-----------");
+                        Directory.CreateDirectory($"{Globals.dataPath}\\emulator\\xenia\\content\\584111F7\\000B0000\\{dj.version}\\{path}");
+                        File.Copy($"{file.FullName}", $"{Globals.dataPath}\\emulator\\xenia\\content\\584111F7\\000B0000\\{dj.version}\\{fileName}");
+                    }
+
+                    Process proc = new Process();
+                    proc.EnableRaisingEvents = true;
+                    proc.StartInfo.FileName = $"\"{Globals.dataPath}\\emulator\\xenia\\xenia_canary.exe\"";
+                    proc.StartInfo.Arguments = $"\"{Globals.dataPath}\\versions\\x360\\tu0\\default.xex\"";
+                    proc.Exited += OnProcessExited;
+                    proc.Start();
+                    Logger.Info("[XboxLauncher]", "Launched xenia @ update");
+                    Discord.ChangeMessage($"Playing Xbox 360 Edition ({dj.version})");
                 }
             }
 
