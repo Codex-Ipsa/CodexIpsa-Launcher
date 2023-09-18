bba86df9955a4067b410f1c556441e2757eb8c1a
DEJVOSS <63589160+IronBeagle2@users.noreply.github.com>
2023-09-18 19:25:46 +0200

    load forge versions

--- MCLauncher/forms/ModLoaders.Designer.cs
+++ MCLauncher/forms/ModLoaders.Designer.cs
@@ -31,6 +31,7 @@
             this.listView1 = new System.Windows.Forms.ListView();
             this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // listView1
@@ -38,11 +39,12 @@
             this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
             this.columnHeader1,
             this.columnHeader2});
-            this.listView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listView1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.listView1.FullRowSelect = true;
             this.listView1.HideSelection = false;
             this.listView1.Location = new System.Drawing.Point(0, 0);
             this.listView1.Name = "listView1";
-            this.listView1.Size = new System.Drawing.Size(284, 291);
+            this.listView1.Size = new System.Drawing.Size(309, 259);
             this.listView1.TabIndex = 0;
             this.listView1.UseCompatibleStateImageBehavior = false;
             this.listView1.View = System.Windows.Forms.View.Details;
@@ -57,11 +59,22 @@
             this.columnHeader2.Text = "Released";
             this.columnHeader2.Width = 150;
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 265);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(285, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Install selected";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
             // ModLoaders
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 291);
+            this.ClientSize = new System.Drawing.Size(309, 293);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.listView1);
             this.Name = "ModLoaders";
             this.Text = "ModLoaders";
@@ -74,5 +87,6 @@
         private System.Windows.Forms.ListView listView1;
         private System.Windows.Forms.ColumnHeader columnHeader1;
         private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.Button button1;
     }
 }
--- MCLauncher/forms/ModLoaders.cs
+++ MCLauncher/forms/ModLoaders.cs
@@ -1,9 +1,11 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Security;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -12,9 +14,47 @@ namespace MCLauncher.forms
 {
     public partial class ModLoaders : Form
     {
+        public List<ModLoaderManifest> mj;
+
         public ModLoaders(string url)
         {
             InitializeComponent();
+
+            string jsonFile = Globals.client.DownloadString(url);
+            mj = JsonConvert.DeserializeObject<List<ModLoaderManifest>>(jsonFile);
+            foreach(ModLoaderManifest m in mj)
+            {
+                if(m.name == "forge")
+                {
+                    foreach(ModLoaderItem item in m.items)
+                    {
+                        listView1.Items.Add(new ListViewItem(new[] { item.id, item.released }));
+                    }
+                }
+            }
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Console.WriteLine(listView1.SelectedItems[0].Index);
+            //mj[0].items[listView1.SelectedItems[0].Index].id
+            //TODO
+        }
+    }
+
+    public class ModLoaderManifest
+    {
+        public string name { get; set; }
+        public ModLoaderItem[] items { get; set; }
+    }
+
+    public class ModLoaderItem
+    {
+        public string id { get; set; }
+        public string json { get; set; }
+        public string type { get; set; }
+        public string url { get; set; }
+        public string released { get; set; }
+        public bool recommended { get; set; }
     }
 }
