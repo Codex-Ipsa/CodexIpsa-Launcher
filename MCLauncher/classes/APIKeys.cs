edcb1e16f76d0ef1194de2e58400bbc775feaa04
DEJVOSS <63589160+IronBeagle2@users.noreply.github.com>
2023-09-18 18:11:44 +0200

    apikeys

--- MCLauncher/classes/APIKeys.cs
+++ MCLauncher/classes/APIKeys.cs
@@ -8,6 +8,7 @@ namespace MCLauncher.classes
 {
     public static partial class APIKeys
     {
+        //these are not here on purpose, so I don't leak any private key shit
         public static readonly string DiscordClientID = "";
     }
 }
