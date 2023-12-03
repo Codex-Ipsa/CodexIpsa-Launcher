edaf9635d0584c3eeb919514a3e8cf31c56f4512
DEJVOSS <63589160+IronBeagle2@users.noreply.github.com>
2023-12-03 18:35:31 +0100

    [pallas] more ui changes

--- MCLauncher/forms/PallasRepo.Designer.cs
+++ MCLauncher/forms/PallasRepo.Designer.cs
@@ -35,6 +35,10 @@
             this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.label1 = new System.Windows.Forms.Label();
             this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
             this.SuspendLayout();
             // 
             // modListView
@@ -44,7 +48,7 @@
             this.modListView.FullRowSelect = true;
             this.modListView.HeaderStyle = System.Windows.Forms.ColumnHeaderStyle.None;
             this.modListView.HideSelection = false;
-            this.modListView.Location = new System.Drawing.Point(12, 12);
+            this.modListView.Location = new System.Drawing.Point(6, 6);
             this.modListView.MultiSelect = false;
             this.modListView.Name = "modListView";
             this.modListView.Size = new System.Drawing.Size(174, 203);
@@ -55,15 +59,15 @@
             // 
             // webBrowser1
             // 
-            this.webBrowser1.Location = new System.Drawing.Point(192, 12);
+            this.webBrowser1.Location = new System.Drawing.Point(192, 6);
             this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
             this.webBrowser1.Name = "webBrowser1";
-            this.webBrowser1.Size = new System.Drawing.Size(270, 176);
+            this.webBrowser1.Size = new System.Drawing.Size(270, 203);
             this.webBrowser1.TabIndex = 1;
             // 
             // downloadButton
             // 
-            this.downloadButton.Location = new System.Drawing.Point(389, 221);
+            this.downloadButton.Location = new System.Drawing.Point(8, 216);
             this.downloadButton.Name = "downloadButton";
             this.downloadButton.Size = new System.Drawing.Size(75, 23);
             this.downloadButton.TabIndex = 2;
@@ -75,7 +79,7 @@
             // 
             this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(277, 194);
+            this.comboBox1.Location = new System.Drawing.Point(273, 218);
             this.comboBox1.Name = "comboBox1";
             this.comboBox1.Size = new System.Drawing.Size(187, 21);
             this.comboBox1.TabIndex = 4;
@@ -83,7 +87,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(192, 197);
+            this.label1.Location = new System.Drawing.Point(190, 221);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(77, 13);
             this.label1.TabIndex = 5;
@@ -93,21 +97,44 @@
             // 
             this.columnHeader1.Width = 135;
             // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(476, 271);
+            this.tabControl1.TabIndex = 6;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.modListView);
+            this.tabPage1.Controls.Add(this.downloadButton);
+            this.tabPage1.Controls.Add(this.comboBox1);
+            this.tabPage1.Controls.Add(this.label1);
+            this.tabPage1.Controls.Add(this.webBrowser1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(468, 245);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Codex-Ipsa";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
             // PallasRepo
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(476, 253);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.comboBox1);
-            this.Controls.Add(this.downloadButton);
-            this.Controls.Add(this.webBrowser1);
-            this.Controls.Add(this.modListView);
+            this.ClientSize = new System.Drawing.Size(476, 271);
+            this.Controls.Add(this.tabControl1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "PallasRepo";
             this.Text = "Mod repository";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -119,5 +146,7 @@
         private System.Windows.Forms.ComboBox comboBox1;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
     }
 }
