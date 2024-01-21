949d3ef4a336c4ab2b27a2cfc7c53137ac80e3df
DEJVOSS <63589160+IronBeagle2@users.noreply.github.com>
2024-01-21 18:20:59 +0100

    images test

--- MCLauncher/controls/ProfileScreen.Designer.cs
+++ MCLauncher/controls/ProfileScreen.Designer.cs
@@ -36,8 +36,9 @@
             // 
             this.listView1.HideSelection = false;
             this.listView1.Location = new System.Drawing.Point(3, 3);
+            this.listView1.MultiSelect = false;
             this.listView1.Name = "listView1";
-            this.listView1.Size = new System.Drawing.Size(650, 381);
+            this.listView1.Size = new System.Drawing.Size(630, 381);
             this.listView1.TabIndex = 0;
             this.listView1.UseCompatibleStateImageBehavior = false;
             // 
--- MCLauncher/controls/ProfileScreen.cs
+++ MCLauncher/controls/ProfileScreen.cs
@@ -17,24 +17,40 @@ namespace MCLauncher.controls
         {
             InitializeComponent();
 
+            reloadProfileList();
+        }
+
+        public void reloadProfileList()
+        {
             ImageList iList = new ImageList();
             iList.ImageSize = new Size(32, 32);
             iList.ColorDepth = ColorDepth.Depth32Bit;
             iList.Images.Add(Image.FromFile("D:\\Source Code\\MineC-raft-Launcher\\MCLauncher\\image\\icon.png"));
+            iList.Images.Add(Image.FromFile("D:\\Source Code\\MineC-raft-Launcher\\MCLauncher\\image\\grass.png"));
+            iList.Images.Add(Image.FromFile("D:\\Source Code\\MineC-raft-Launcher\\MCLauncher\\image\\552552.png"));
+            iList.Images.Add(Image.FromFile("D:\\Source Code\\MineC-raft-Launcher\\MCLauncher\\image\\5525521.png"));
+            iList.Images.Add(Image.FromFile("D:\\Source Code\\MineC-raft-Launcher\\MCLauncher\\image\\Omniarchive.png"));
 
             listView1.LargeImageList = iList;
 
             string[] dirs = Directory.GetDirectories($"{Globals.dataPath}\\instance\\", "*");
 
+            int i = 0;
             foreach (string dir in dirs)
             {
                 var dirN = new DirectoryInfo(dir);
                 var dirName = dirN.Name;
                 if (File.Exists($"{Globals.dataPath}\\instance\\{dirName}\\instance.json"))
                 {
-                    listView1.Items.Add(new ListViewItem { ImageIndex = 0, Text = dirName });
+                    listView1.Items.Add(new ListViewItem { ImageIndex = i, Text = dirName });
                 }
+
+                i++;
+
+                if (i == 5)
+                    i = 0;
             }
+
         }
     }
 }
