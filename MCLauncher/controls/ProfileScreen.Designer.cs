8b0c82f5da56b28c76cbd22df271dcbe19a0bfd3
DEJVOSS <63589160+IronBeagle2@users.noreply.github.com>
2024-01-22 19:35:32 +0100

    add buttons

--- MCLauncher/controls/ProfileScreen.Designer.cs
+++ MCLauncher/controls/ProfileScreen.Designer.cs
@@ -32,6 +32,9 @@
             this.listView1 = new System.Windows.Forms.ListView();
             this.iconPanel = new System.Windows.Forms.Panel();
             this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // listView1
@@ -64,11 +67,41 @@
             this.label1.Text = "Ready to play INSTANCE";
             this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(667, 118);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(85, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Play";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(667, 147);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(85, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Edit profile";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(667, 361);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(85, 23);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "New profile";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
             // ProfileScreen
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.iconPanel);
             this.Controls.Add(this.listView1);
@@ -83,5 +116,8 @@
         private System.Windows.Forms.ListView listView1;
         private System.Windows.Forms.Panel iconPanel;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
     }
 }
--- MCLauncher/controls/ProfileScreen.cs
+++ MCLauncher/controls/ProfileScreen.cs
@@ -1,14 +1,8 @@
 ﻿using MCLauncher.classes;
 using MCLauncher.Properties;
 using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace MCLauncher.controls
