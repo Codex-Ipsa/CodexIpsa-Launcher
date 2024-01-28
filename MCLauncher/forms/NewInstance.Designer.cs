516e8762c9eaf12f84bc79f0bb8402514780bf80
DEJVOSS <63589160+IronBeagle2@users.noreply.github.com>
2024-01-28 20:55:06 +0100

    more ui

--- MCLauncher/forms/NewInstance.Designer.cs
+++ MCLauncher/forms/NewInstance.Designer.cs
@@ -144,7 +144,7 @@
             // 
             this.textBox1.Location = new System.Drawing.Point(98, 6);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(348, 20);
+            this.textBox1.Size = new System.Drawing.Size(464, 20);
             this.textBox1.TabIndex = 2;
             // 
             // label1
