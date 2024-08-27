9eb8254ec30c96f926e1b7b05ac62839dbc8d60c
DEJVOSS <63589160+IronBeagle2@users.noreply.github.com>
2024-08-27 15:50:13 +0200

    FabricParser (classes)

--- MCLauncher/classes/FabricParser.cs
+++ MCLauncher/classes/FabricParser.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 
-namespace MCLauncher.classes.ipsajson
+namespace MCLauncher.classes
 {
     internal class FabricParser
     {
