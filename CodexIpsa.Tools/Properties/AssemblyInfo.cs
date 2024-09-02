33fb49b0ee6fc2cc0f2740b40a5ee896702e2d0f
DEJVOSS <63589160+IronBeagle2@users.noreply.github.com>
2024-09-02 16:35:34 +0200

    Add CodexIpsa.Tools project

---
+++ CodexIpsa.Tools/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Obecné informace o sestavení se řídí přes následující 
+// sadu atributů. Změnou hodnot těchto atributů se upraví informace
+// přidružené k sestavení.
+[assembly: AssemblyTitle("CodexIpsa.Tools")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CodexIpsa.Tools")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Nastavení ComVisible na false způsobí neviditelnost typů v tomto sestavení
+// pro komponenty modelu COM. Pokud potřebujete přístup k typu v tomto sestavení
+// komponenty COM, nastavte atribut ComVisible daného typu na hodnotu true.
+[assembly: ComVisible(false)]
+
+// Následující GUID se používá pro ID knihovny typů, pokud je tento projekt vystavený pro COM.
+[assembly: Guid("33b4e004-960b-435d-b286-c154b3d27b3e")]
+
+// Informace o verzi sestavení se skládá z těchto čtyř hodnot:
+//
+//      Hlavní verze
+//      Podverze
+//      Číslo sestavení
+//      Revize
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
