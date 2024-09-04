d1a92afceddd76dff65f9b9745e0c34806940df5
DEJVOSS <63589160+IronBeagle2@users.noreply.github.com>
2024-09-04 18:12:50 +0200

    add note about APIKeysLocal.cs

--- MCLauncher/classes/APIKeys.cs
+++ MCLauncher/classes/APIKeys.cs
@@ -3,6 +3,15 @@
     public static partial class APIKeys
     {
         //these are not here on purpose, so I don't leak any private key shit
+        //create APIKeysLocal.cs and make a constructor like this
+        /*public static partial class APIKeys
+        {
+            static APIKeys()
+            {
+                DiscordClientID = "KEY GOES HERE";
+            }
+        }*/
+
         public static readonly string DiscordClientID = "";
     }
 }
