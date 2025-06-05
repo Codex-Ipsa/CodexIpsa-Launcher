40f40bb639e9d67c08d5e14d61d517545a2711bc
DEJVOSS <63589160+IronBeagle2@users.noreply.github.com>
2025-06-05 17:00:51 +0200

    fix loading a local assetindex json

--- MCLauncher/classes/JavaLauncher.cs
+++ MCLauncher/classes/JavaLauncher.cs
@@ -130,6 +130,7 @@ namespace MCLauncher.classes
                     if (!vj.assets.url.Contains("http"))
                     {
                         vj.assets.url = "file:///" + vj.assets.url;
+                        vj.assets.name = ij.assetsPath.Substring(ij.assetsPath.LastIndexOf('\\') + 1).Replace(".json", "");
                     }
 
                     AssetsDownloader ad = new AssetsDownloader(vj.assets.url, vj.assets.name);
