5f308f61b753f40eced9fd9414a032d2e75798c4
DEJVOSS <63589160+IronBeagle2@users.noreply.github.com>
2025-09-24 19:47:20 +0200

    press enter in ip window to start

--- MCLauncher/forms/EnterIp.Designer.cs
+++ MCLauncher/forms/EnterIp.Designer.cs
@@ -54,6 +54,7 @@
             this.comboBox1.Name = "comboBox1";
             this.comboBox1.Size = new System.Drawing.Size(249, 21);
             this.comboBox1.TabIndex = 1;
+            this.comboBox1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.comboBox1_KeyDown);
             // 
             // button1
             // 
